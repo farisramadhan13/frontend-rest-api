7364b166f583b969d0f0808770c7eedd54902498
farisramadhan13 <farisramadhan345@gmail.com>
2024-06-14 07:39:14 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full flex justify-start z-10 items-center text-center font-black text-3xl p-5 fixed top-0 right-0 left-0 drop-shadow-lg text-white bg-purple-800'>
+    <div className='w-full flex justify-start z-10 items-center text-center font-black text-3xl p-4 fixed top-0 right-0 left-0 drop-shadow-lg text-white bg-purple-800'>
       CONTOH REST API
     </div>
   )
